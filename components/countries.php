dac936e7dd6df33a44c2d47a3fd413c14fdb566a
Tsovinar <tsovinar.nemesida.grigoryan@gmail.com>
2018-08-13 22:40:47 +0400

    sql total count of countries

--- components/countries.php
+++ components/countries.php
@@ -256,9 +256,6 @@
 //INSERT INTO countries(title) VALUES ( 'Anguilla');
 //INSERT INTO countries(title) VALUES ( 'Antarctica');
 //INSERT INTO countries(title) VALUES ( 'Antigua and Barbuda');
-
-
-
 //INSERT INTO countries(title) VALUES ( 'Argentina');
 //INSERT INTO countries(title) VALUES ( 'Armenia');
 //INSERT INTO countries(title) VALUES ( 'Aruba');
@@ -277,10 +274,6 @@
 //INSERT INTO countries(title) VALUES ( 'Bhutan');
 //INSERT INTO countries(title) VALUES ( 'Bolivia');
 //INSERT INTO countries(title) VALUES ('Bosnia and Herzegowina');
-
-
-
-//
 //INSERT INTO countries(title) VALUES ( 'Botswana');
 //INSERT INTO countries(title) VALUES ( 'Bouvet Island');
 //INSERT INTO countries(title) VALUES ( 'Brazil');
@@ -290,8 +283,6 @@
 //INSERT INTO countries(title) VALUES ( 'Burkina Faso');
 //INSERT INTO countries(title) VALUES (  'Burundi');
 //INSERT INTO countries(title) VALUES ( 'Cambodia');
-
-
 //INSERT INTO countries(title) VALUES ('Cameroon');
 //INSERT INTO countries(title) VALUES ( 'Canada');
 //INSERT INTO countries(title) VALUES ( 'Cape Verde');
@@ -301,8 +292,6 @@
 //INSERT INTO countries(title) VALUES ( 'Chile');
 //INSERT INTO countries(title) VALUES (  'China');
 //INSERT INTO countries(title) VALUES ( 'Christmas Island');
-
-//
 //INSERT INTO countries(title) VALUES ('Cocos (Keeling) Islands');
 //INSERT INTO countries(title) VALUES ( 'Colombia');
 //INSERT INTO countries(title) VALUES ( 'Comoros');
@@ -321,3 +310,187 @@
 //INSERT INTO countries(title) VALUES ('Dominican Republic');
 //INSERT INTO countries(title) VALUES (  'East Timor');
 //INSERT INTO countries(title) VALUES (  'Ecuador');
+//INSERT INTO countries(title) VALUES ('Egypt');
+//INSERT INTO countries(title) VALUES ( 'El Salvador');
+//INSERT INTO countries(title) VALUES ('Equatorial Guinea');
+//INSERT INTO countries(title) VALUES ( 'Eritrea');
+//INSERT INTO countries(title) VALUES ( 'Estonia');
+//INSERT INTO countries(title) VALUES ('Ethiopia');
+//INSERT INTO countries(title) VALUES ( 'Falkland Islands (Malvinas)');
+//INSERT INTO countries(title) VALUES ( 'Faroe Islands');
+//INSERT INTO countries(title) VALUES ('Fiji');
+//INSERT INTO countries(title) VALUES ( 'Finland');
+//INSERT INTO countries(title) VALUES ( 'France');
+//INSERT INTO countries(title) VALUES ( 'France Metropolitan');
+//INSERT INTO countries(title) VALUES ('French Guiana');
+//INSERT INTO countries(title) VALUES (  'French Polynesia');
+//INSERT INTO countries(title) VALUES ('French Southern Territories');
+//INSERT INTO countries(title) VALUES ('Gabon');
+//INSERT INTO countries(title) VALUES (   'Gambia');
+//INSERT INTO countries(title) VALUES ( 'Georgia');
+//INSERT INTO countries(title) VALUES ( 'Germany');
+//INSERT INTO countries(title) VALUES ('Ghana');
+//INSERT INTO countries(title) VALUES ( 'Gibraltar');
+//INSERT INTO countries(title) VALUES ( 'Greece');
+//INSERT INTO countries(title) VALUES ('Greenland');
+//INSERT INTO countries(title) VALUES ( 'Grenada');
+//INSERT INTO countries(title) VALUES ( 'Guadeloupe');
+//INSERT INTO countries(title) VALUES ('Guam');
+//INSERT INTO countries(title) VALUES ( 'Guatemala');
+//INSERT INTO countries(title) VALUES ( 'Guinea');
+//INSERT INTO countries(title) VALUES ( 'Haiti');
+//INSERT INTO countries(title) VALUES ('Heard and Mc Donald Islands');
+//INSERT INTO countries(title) VALUES ( 'Holy See (Vatican City State)');
+//INSERT INTO countries(title) VALUES ('Honduras');
+//INSERT INTO countries(title) VALUES ('Hong Kong');
+//INSERT INTO countries(title) VALUES ('Hungary');
+//INSERT INTO countries(title) VALUES (  'Iceland');
+//INSERT INTO countries(title) VALUES ( 'India');
+//INSERT INTO countries(title) VALUES ('Indonesia');
+//INSERT INTO countries(title) VALUES ( 'Iran (Islamic Republic of)');
+//INSERT INTO countries(title) VALUES ( 'Iraq');
+//INSERT INTO countries(title) VALUES ('Ireland');
+//INSERT INTO countries(title) VALUES ( 'Israel');
+//INSERT INTO countries(title) VALUES ( 'Italy');
+//INSERT INTO countries(title) VALUES ('Jamaica');
+//INSERT INTO countries(title) VALUES ( 'Japan');
+//INSERT INTO countries(title) VALUES ( 'Jordan');
+//INSERT INTO countries(title) VALUES ('Kazakhstan');
+//INSERT INTO countries(title) VALUES ('Kenya');
+//INSERT INTO countries(title) VALUES ( 'Kiribati');
+//INSERT INTO countries(title) VALUES ('Korea, Democratic People\'s Republic of');
+//INSERT INTO countries(title) VALUES ('Korea, Republic of');
+//INSERT INTO countries(title) VALUES ('Kuwait');
+//INSERT INTO countries(title) VALUES ('Kyrgyzstan');
+//INSERT INTO countries(title) VALUES ( 'Lao, People\'s Democratic Republic');
+//INSERT INTO countries(title) VALUES ( 'Latvia');
+//INSERT INTO countries(title) VALUES ('Lebanon');
+//INSERT INTO countries(title) VALUES ( 'Lesotho');
+//INSERT INTO countries(title) VALUES ( 'Liberia');
+//INSERT INTO countries(title) VALUES ('Libyan Arab Jamahiriya');
+//INSERT INTO countries(title) VALUES ('Liechtenstein');
+//INSERT INTO countries(title) VALUES ('Lithuania');
+//INSERT INTO countries(title) VALUES ( 'Luxembourg');
+//INSERT INTO countries(title) VALUES ( 'Macau');
+//INSERT INTO countries(title) VALUES ('Macedonia, The Former Yugoslav Republic of');
+//INSERT INTO countries(title) VALUES ('Madagascar');
+//INSERT INTO countries(title) VALUES ('Malawi');
+//INSERT INTO countries(title) VALUES ('Maldives');
+//INSERT INTO countries(title) VALUES ( 'Mali');
+//INSERT INTO countries(title) VALUES ('Malta');
+//INSERT INTO countries(title) VALUES ('Marshall Islands');
+//INSERT INTO countries(title) VALUES ( 'Martinique');
+//INSERT INTO countries(title) VALUES ( 'Mauritania');
+//INSERT INTO countries(title) VALUES ('Mauritius');
+//INSERT INTO countries(title) VALUES ('Mayotte');
+//INSERT INTO countries(title) VALUES ('Mexico');
+//INSERT INTO countries(title) VALUES ( 'Micronesia, Federated States of');
+//INSERT INTO countries(title) VALUES ( 'Moldova, Republic of');
+//INSERT INTO countries(title) VALUES ('Monaco');
+//INSERT INTO countries(title) VALUES ('Mongolia');
+//INSERT INTO countries(title) VALUES ( 'Montserrat');
+//INSERT INTO countries(title) VALUES ( 'Morocco');
+//INSERT INTO countries(title) VALUES ( 'Mozambique');
+//INSERT INTO countries(title) VALUES ( 'Myanmar');
+//INSERT INTO countries(title) VALUES ( 'Namibia');
+//INSERT INTO countries(title) VALUES ('Nauru');
+//INSERT INTO countries(title) VALUES ('Nepal');
+//INSERT INTO countries(title) VALUES ( 'Netherlands');
+//INSERT INTO countries(title) VALUES ('Netherlands Antilles');
+//INSERT INTO countries(title) VALUES ('New Caledonia');
+//INSERT INTO countries(title) VALUES ('New Zealand');
+//INSERT INTO countries(title) VALUES ('Nicaragua');
+//INSERT INTO countries(title) VALUES ( 'Niger');
+//INSERT INTO countries(title) VALUES ( 'Nigeria');
+//INSERT INTO countries(title) VALUES ('Niue');
+//INSERT INTO countries(title) VALUES ( 'Norfolk Island');
+//INSERT INTO countries(title) VALUES ( 'Northern Mariana Islands');
+//INSERT INTO countries(title) VALUES ('Norway');
+//INSERT INTO countries(title) VALUES ('Oman');
+//INSERT INTO countries(title) VALUES ('Pakistan');
+//INSERT INTO countries(title) VALUES ( 'Palau');
+//INSERT INTO countries(title) VALUES ('Panama');
+//INSERT INTO countries(title) VALUES ('Papua New Guinea');
+//INSERT INTO countries(title) VALUES ( 'Paraguay');
+//INSERT INTO countries(title) VALUES ('Peru');
+//INSERT INTO countries(title) VALUES ( 'Philippines');
+//INSERT INTO countries(title) VALUES ( 'Pitcairn');
+//INSERT INTO countries(title) VALUES ( 'Poland');
+//INSERT INTO countries(title) VALUES ('Portugal');
+//INSERT INTO countries(title) VALUES ('Puerto Rico');
+//INSERT INTO countries(title) VALUES ( 'Qatar');
+//INSERT INTO countries(title) VALUES ('Reunion');
+//INSERT INTO countries(title) VALUES ('Romania');
+//INSERT INTO countries(title) VALUES ('Russian Federation');
+//INSERT INTO countries(title) VALUES ( 'Rwanda');
+//INSERT INTO countries(title) VALUES ('Saint Kitts and Nevis');
+//INSERT INTO countries(title) VALUES ('Saint Lucia');
+//INSERT INTO countries(title) VALUES (  'Saint Vincent and the Grenadines');
+//INSERT INTO countries(title) VALUES ('Samoa');
+//INSERT INTO countries(title) VALUES ('San Marino');
+//INSERT INTO countries(title) VALUES ('Sao Tome and Principe');
+//INSERT INTO countries(title) VALUES ( 'Saudi Arabia');
+//INSERT INTO countries(title) VALUES ('Senegal');
+//INSERT INTO countries(title) VALUES ('Seychelles');
+//INSERT INTO countries(title) VALUES ('Sierra Leone');
+//INSERT INTO countries(title) VALUES ('Singapore');
+//INSERT INTO countries(title) VALUES ('Slovakia (Slovak Republic)');
+//INSERT INTO countries(title) VALUES ('Slovenia');
+//INSERT INTO countries(title) VALUES ('Solomon Islands');
+//INSERT INTO countries(title) VALUES ('Somalia');
+//INSERT INTO countries(title) VALUES ('South Africa');
+//INSERT INTO countries(title) VALUES ('South Georgia and the South Sandwich Islands');
+//INSERT INTO countries(title) VALUES ('Spain');
+//INSERT INTO countries(title) VALUES ('Sri Lanka');
+//INSERT INTO countries(title) VALUES ('St. Helena');
+//INSERT INTO countries(title) VALUES ('St. Pierre and Miquelon');
+//INSERT INTO countries(title) VALUES ( 'Sudan');
+//INSERT INTO countries(title) VALUES ('Suriname');
+//INSERT INTO countries(title) VALUES ('Svalbard and Jan Mayen Islands');
+//INSERT INTO countries(title) VALUES ('Swaziland');
+//INSERT INTO countries(title) VALUES ('Sweden');
+//INSERT INTO countries(title) VALUES ('Switzerland');
+//INSERT INTO countries(title) VALUES ('Syrian Arab Republic');
+//INSERT INTO countries(title) VALUES ('Taiwan, Province of China');
+//INSERT INTO countries(title) VALUES ('Tajikistan');
+//INSERT INTO countries(title) VALUES ('Tanzania, United Republic of');
+//INSERT INTO countries(title) VALUES ('Thailand');
+//INSERT INTO countries(title) VALUES ( 'Togo');
+//INSERT INTO countries(title) VALUES ( 'Tokelau');
+//INSERT INTO countries(title) VALUES ('Tonga');
+//INSERT INTO countries(title) VALUES ('Trinidad and Tobago');
+//INSERT INTO countries(title) VALUES ('Tunisia');
+//INSERT INTO countries(title) VALUES ('Turkey');
+//INSERT INTO countries(title) VALUES ('Turkmenistan');
+//INSERT INTO countries(title) VALUES ('Turks and Caicos Islands');
+//INSERT INTO countries(title) VALUES ('Tuvalu');
+//INSERT INTO countries(title) VALUES ('Uganda');
+//INSERT INTO countries(title) VALUES ('Ukraine');
+//INSERT INTO countries(title) VALUES ( 'United Arab Emirates');
+//INSERT INTO countries(title) VALUES ('United Kingdom');
+//INSERT INTO countries(title) VALUES ('United States');
+//INSERT INTO countries(title) VALUES ('United States Minor Outlying Islands');
+//INSERT INTO countries(title) VALUES ( 'Uruguay');
+//INSERT INTO countries(title) VALUES ('Uzbekistan');
+//INSERT INTO countries(title) VALUES ('Vanuatu');
+//INSERT INTO countries(title) VALUES ('Venezuela');
+//INSERT INTO countries(title) VALUES ('Vietnam');
+//INSERT INTO countries(title) VALUES ( 'Virgin Islands (British)');
+//INSERT INTO countries(title) VALUES ('Virgin Islands (U.S.)');
+//INSERT INTO countries(title) VALUES ('Wallis and Futuna Islands');
+//INSERT INTO countries(title) VALUES ('Western Sahara');
+//INSERT INTO countries(title) VALUES ('Yemen');
+//INSERT INTO countries(title) VALUES ( 'Yugoslavia');
+//INSERT INTO countries(title) VALUES ('Zambia');
+//INSERT INTO countries(title) VALUES ('Zimbabwe');
+
+
+
+
+
+
+
+
+
+
+
--- layouts/left-sidebar.php
+++ layouts/left-sidebar.php
@@ -7,7 +7,7 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <div class="container">
     <div class="row">
         <div class="col-md-3 left">
-            <h3>Categories</h3>
+            <h3 style="font-style: italic">Categories</h3>
             <ul class="category_nav" id="sortable">
                 <?php foreach ($data as $value):?>
                     <li><a href="category.php?id=<?= $value['id']
